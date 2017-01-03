eca41ae097e5961ebf1275efdab4fb96b387ea8f
wangzengdi <sunnyadi@163.com>
2017-01-03 17:53:45 +0800

    graphql schema language buildSchema

--- hello_server.js
+++ hello_server.js
@@ -1,11 +1,5 @@
 import express from 'express';
-import {
-  graphql,
-  buildSchema,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString,
-  } from 'graphql';
+import { graphql, buildSchema } from 'graphql';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -13,24 +7,19 @@ const PORT = 3000;
 // parse POST body as text
 app.use(bodyParser.text({ type: 'application/graphql' }));
 
-const queryType = new GraphQLObjectType({
-  name: 'Query',
-  fields: {
-    hello: {
-      type: GraphQLString,
-      args: {
-        str: { type: GraphQLString },
-      },
-      resolve: (rootValue, { str }) => `hello, ${str}`,
-    },
-  },
-});
+const schema = buildSchema(`
+  type Query {
+    hello(str: String): String 
+  }
+`);
 
-const schema = new GraphQLSchema({ query: queryType });
+export const root = {
+  hello: rootValue => `hello, ${rootValue.str}`,
+};
 
 app.post('/graphql', (req, res) => {
   // execute GraphQL!
-  graphql(schema, req.body).then((response) => {
+  graphql(schema, req.body, root).then((response) => {
     res.send(JSON.stringify(response, null, 2));
   });
 });
@@ -40,14 +29,4 @@ const server = app.listen(PORT, () => {
   const port = server.address().port;
   console.log('GraphQL listening at http://%s:%s', host, port);
 });
-
-// const schema = buildSchema(`
-//   type Query {
-//     hello(str: String): String
-//   }
-// `);
-//
-// export const root = {
-//   hello: rootValue => `hello, ${rootValue.str}`,
-// };
 
