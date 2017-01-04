b0d1bbf7bc04f782504c7c943b5fdc084f753001
wangzengdi <sunnyadi@163.com>
2017-01-04 09:44:13 +0800

    add server.js graphiql

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { graphql } from 'graphql';
-import bodyParser from 'body-parser';
+import graphqlHTTP from 'express-graphql';
 
 import {
   schema as schemaNormal,
@@ -15,32 +15,13 @@ import {
 } from './schema/starWarsSchema';
 
 const app = express();
-const PORT = 3000;
 
-// parse POST body as text
-app.use(bodyParser.text({ type: 'application/graphql' }));
+const schema = StarWarsSchema;
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
-app.post('/graphql', (req, res) => {
-  // execute GraphQL!
-  console.log('**********req.body', req.body);
-  const schema = StarWarsSchema;
-//  graphql(schemaNormal, req.body)
-//  .then((result) => {
-//    res.send(JSON.stringify(result, null, 2));
-//  });
-//  graphql(schemaShort, req.body, root).then((response) => {
-//    res.send(JSON.stringify(response, null, 2));
-//  });
-  graphql(schema, req.body).then((response) => {
-    console.log(response);
-    res.send(JSON.stringify(response, null, 2));
-  });
-});
-
-const server = app.listen(PORT, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('GraphQL listening at http://%s:%s', host, port);
-});
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
 
